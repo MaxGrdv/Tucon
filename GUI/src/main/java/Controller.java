0f8f059dc6c80716a568c3a99e98680b07664d9e
gigel773 <gigelbig@gmail.com>
2017-03-27 17:18:08 +0300

    GUI configured

--- GUI/src/main/java/Controller.java
+++ GUI/src/main/java/Controller.java
@@ -1,15 +1,75 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import model.Client;
+import server.Info;
+
+import java.util.LinkedList;
 
 public class Controller {
 
     @FXML
     private Button button;
+    @FXML
+    private TextField inputMessage;
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField getterName;
+    @FXML
+    private TextArea chat;
+    @FXML
+    private Button login;
+
+    private Client user;
 
     @FXML
-    void press() {
+    public void logIn() throws Exception {
 
-        // TODO
+        user = new Client((String) username.getText());
+
+        Thread gettingMessages = new Thread(new Runnable() {
+
+            public void run() {
+
+                while (true) {
+
+                    LinkedList<Info> newMessages = user.getMessages();
+
+                    for (Info each : newMessages) {
+
+                        chat.appendText(each.getSender() + ": " + each.getMessage() + "\n");
+
+                    }
+                }
+            }
+        });
+
+        gettingMessages.start();
+
+    }
+
+    @FXML
+    int sendMessage() throws Exception {
+
+        String message = inputMessage.getText();
+
+        chat.appendText(user.getUsername() + ": " + message + "\n");
+
+        inputMessage.setText("");
+
+        if ("".equals(message)) {
+
+            return 0;
+
+        } else {
+
+            user.sendMessage(getterName.getText(), message);
+
+        }
+
+        return 1;
 
     }
 
